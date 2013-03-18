9e7dc59a2303f6416fbce3632b25e0eebdf86787
Loic Sharma <sharma.loic@gmail.com>
2013-03-18 01:46:31 -0400

    Cleaning up the variable class.

--- src/Compiler/Languages/Variables/Variable.php
+++ src/Compiler/Languages/Variables/Variable.php
@@ -2,15 +2,38 @@
 
 class Variable {
 
+	/**
+	 * The variable's name.
+	 *
+	 * @var string
+	 */
 	public $name;
+
+	/**
+	 * The variable's value.
+	 *
+	 * @var mixed
+	 */
 	public $value;
 
+	/**
+	 * Create the new variable.
+	 *
+	 * @param  string  $name
+	 * @param  mixed   $value
+	 */
 	public function __construct($name, $value = 0)
 	{
 		$this->name = $name;
 		$this->value = $value;
 	}
 
+	/**
+	 * Render the variable.
+	 *
+	 * @todo Make the class abstract and remove this method.
+	 * @return string
+	 */
 	// abstract public function __toString();
 	public function __toString()
 	{
