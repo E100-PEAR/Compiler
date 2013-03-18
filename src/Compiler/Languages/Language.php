0afa74a7b84d58a99957e5ce8611f6ed1a1001d8
Loic Sharma <sharma.loic@gmail.com>
2013-03-18 02:03:54 -0400

    Cleaning up.

--- src/Compiler/Languages/Language.php
+++ src/Compiler/Languages/Language.php
@@ -1,22 +1,58 @@
 <?php namespace Compiler\Languages;
 
+use Compiler\Compiler;
+use Compiler\Languages\Variables\Variables;
+
 abstract class Language {
 
+	/**
+	 * The current language's compiler.
+	 *
+	 * @var Compiler\Compiler
+	 */
 	protected $compiler;
 
+	/**
+	 * The currently defined language variables.
+	 *
+	 * @var Compiler\Languages\Variables\Variables
+	 */
 	public $variables = array();
 
-	public function __construct($variables)
+	/**
+	 * Register the current language and its variables.
+	 *
+	 * @param  Compiler\Languages\Variables\Variables  $variables
+	 * @return void
+	 */
+	public function __construct(Variables $variables)
 	{
 		$this->variables = $variables;
 	}
 
-	public function setCompiler($compiler)
+	/**
+	 * Set the current language's compiler.
+	 *
+	 * @param  Compiler\Compiler $compiler
+	 * @return void
+	 */
+	public function setCompiler(Compiler $compiler)
 	{
 		$this->compiler = $compiler;
 	}
 
+	/**
+	 * Get a translator from the current language.
+	 *
+	 * @param  string                         $translator
+	 * @return Compiler\Languages\Translator
+	 */
 	abstract public function getTranslator($translator);
 
+	/**
+	 * Get the current translation for the compiled code.
+	 *
+	 * @return string
+	 */
 	abstract public function getTranslation();
 }
