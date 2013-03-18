255fc2c7c938a27b0a58afadc2013f8ff3a7b97e
Loic Sharma <sharma.loic@gmail.com>
2013-03-17 23:33:14 -0400

    Added an increment expression

---
+++ src/Compiler/Languages/Assembly/Expressions/PostIncTranslator.php
@@ -0,0 +1,15 @@
+<?php namespace Compiler\Languages\Assembly\Expressions;
+
+use Compiler\Languages\Translator;
+
+class PostIncTranslator extends Translator {
+
+	public function translate($expression)
+	{
+		$left  = $this->language->expressionToMemory($expression->var);
+
+		$this->language->addCommand('add', $left, $left, '_int_1');
+
+		return $left;
+	}
+}
