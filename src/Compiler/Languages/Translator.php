de4ed2b6d729c409454b86533b58a4ed1af26e20
Loic Sharma <sharma.loic@gmail.com>
2013-03-18 01:24:36 -0400

    Cleaning up the translator class

--- src/Compiler/Languages/Translator.php
+++ src/Compiler/Languages/Translator.php
@@ -2,14 +2,38 @@
 
 abstract class Translator {
 
+	/**
+	 * The compiler that is translating the tokens.
+	 *
+	 * @var Compiler\Compiler
+	 */
 	protected $compiler;
+
+	/**
+	 * The language that the translator belongs to.
+	 *
+	 * @var Object
+	 */
 	protected $language;
 
+	/**
+	 * Register the translator's compiler and language.
+	 *
+	 * @param  Compiler\Compiler  $compiler
+	 * @param  object             $language
+	 */
 	public function __construct($compiler, $language)
 	{
 		$this->compiler = $compiler;
 		$this->language = $language;
 	}
 
+	/**
+	 * Translate the token. Note that any side-effects will
+	 * be sent to the language object.
+	 *
+	 * @param  mixed   $token
+	 * @return string
+	 */
 	abstract public function translate($token);
 }
