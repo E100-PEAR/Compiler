4d715c44854386e0f58cb635fd95d3f65733e46c
Loic Sharma <sharma.loic@gmail.com>
2013-03-18 01:30:45 -0400

    Cleaning up the variables class.

--- src/Compiler/Languages/Assembly/Variables.php
+++ src/Compiler/Languages/Assembly/Variables.php
@@ -2,15 +2,39 @@
 
 class Variables {
 
+	/**
+	 * The currently defined language variables.
+	 *
+	 * @var array
+	 */
 	protected $variables = array();
 
+	/**
+	 * The currently defined language arrays.
+	 *
+	 * @var array
+	 */
 	protected $arrays = array();
 
+	/**
+	 * Verify that a variable is set.
+	 *
+	 * @param  string  $key
+	 * @return bool
+	 */
 	public function exists($key)
 	{
 		return isset($this->variables[$key]);
 	}
 
+	/**
+	 * Create a new variable or array.
+	 *
+	 * @param  string  $key
+	 * @param  mixed   $value
+	 * @param  bool    $array
+	 * @return void
+	 */
 	public function create($key, $value = 0, $array = false)
 	{
 		if( ! $this->exists($key))
@@ -27,16 +51,37 @@ class Variables {
 		}
 	}
 
+	/**
+	 * Create an array.
+	 *
+	 * @param  string  $key
+	 * @param  array   $value
+	 * @return void
+	 */
 	public function createArray($key, array $values = array())
 	{
 		$this->create($key, $values, true);
 	}
 
+	/**
+	 * Set the value to a variable.
+	 *
+	 * @param  string  $key
+	 * @param  mixed   $value
+	 * @return void
+	 */
 	public function set($key, $value)
 	{
 		$this->variables[$key] = $value;
 	}
 
+	/**
+	 * Get the value of a variable. Returns all variables
+	 * if no key is passed.
+	 *
+	 * @param  string  $key
+	 * @return mixed
+	 */
 	public function get($key = null)
 	{
 		if(is_null($key))
