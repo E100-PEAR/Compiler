06881b63766cbabdf085b76b64cdb2badbcb7a22
Loic Sharma <sharma.loic@gmail.com>
2013-03-18 23:48:42 -0400

    Cleaning up.

--- src/Compiler/Languages/Assembly/Language.php
+++ src/Compiler/Languages/Assembly/Language.php
@@ -11,14 +11,34 @@ use Compiler\Languages\Assembly\Commands\Command;
 
 class Language extends CompilerLanguage {
 
+	/**
+	 * The commands extracted from the AST.
+	 *
+	 * @var array
+	 */
 	protected $commands = array();
 
+	/**
+	 * The current scope of the language.
+	 *
+	 * @todo implement
+	 * @var  array
+	 */
 	public $scope = array();
 
-	// The compiler uses zero's and one's so
-	// we'll always need to include those.
+	/**
+	 * The largest integer that the compiler should generate.
+	 * Note that 0 and 1 are always generated to be used as booleans.
+	 *
+	 * @var int
+	 */
 	public $largestInteger = 1;
 
+	/**
+	 * Get the instance of the language's token translator.
+	 *
+	 * @return Compiler\Languges\Translator
+	 */
 	public function getTranslator($translator)
 	{
 		$translator = 'Compiler\\Languages\\Assembly\\'.$translator.'Translator';
@@ -26,24 +46,41 @@ class Language extends CompilerLanguage {
 		return new $translator($this->compiler, $this);
 	}
 
+	/**
+	 * Add a new compiled command.
+	 *
+	 * @param  string  $type
+	 * @param  mixed   $param1
+	 * @param  mixed   $param2
+	 * @param  mixed   $param3
+	 * @return Compiler\Languages\Assembly\Commands\Command
+	 */
 	public function addCommand($name, $param1 = null, $param2 = null, $param3 = null)
 	{
-		$tag = null;
+		$label = null;
 
 		if(strpos($name, ' ') !== false)
 		{
-			list($tag, $name) = explode(' ', $name);
+			list($label, $name) = explode(' ', $name);
 		}
 
 		$command = new Command;
 
 		$command->setType($name);
 		$command->setParameters($param1, $param2, $param3);
-		$command->setLabel($tag);
+		$command->setLabel($label);
 
 		$this->commands[] = $command;
+
+		return $command;
 	}
 
+	/**
+	 * Convert an expression class into its corresponding memory label.
+	 *
+	 * @param  mixed   $expression
+	 * @return string
+	 */
 	public function expressionToMemory($expression)
 	{
 		// Variables 
@@ -103,21 +140,58 @@ class Language extends CompilerLanguage {
 		return $value;
 	}
 
+	/** 
+	 * Create a command that will redirect the PC to a new location.
+	 *
+	 * @param  string  $location
+	 * @return Compiler\Languages\Assembly\Commands\Command
+	 */
 	public function redirectTo($location)
 	{
-		$this->addCommand('be', $location, '_int_1', '_int_1');
+		return $this->addCommand('be', $location, '_int_1', '_int_1');
 	}
 
+	/**
+	 * Add a useless labeled command that will serve as a marker. This marker
+	 * can be used as a location to redirect the PC to.
+	 *
+	 * @param  string  $name
+	 * @return Compiler\Languages\Assembly\Commands\Command
+	 */
 	public function addMarker($name)
 	{
-		$this->addCommand($name.' cp', '_int_0', '_int_0');
+		return $this->addCommand($name.' cp', '_int_0', '_int_0');
 	}
 
-	public function getMemoryLocationName($input)
+	/**
+	 * Set the current scope.
+	 *
+	 * @todo   Make this actually do something!
+	 * @param  string  $scope
+	 * @return void
+	 */
+	public function setScope($scope)
 	{
-		return $input;
+		$this->addMarker($scope.'_start');
 	}
 
+	/**
+	 * Remove a scope.
+	 *
+	 * @todo   Make this actually do something!
+	 * @param  string  $scope
+	 * @return void
+	 */
+	public function removeScope($scope)
+	{
+		$this->addMarker($scope.'_end');
+	}
+
+	/**
+	 * Get the final translation of the parsed code.
+	 *
+	 * @return string
+	 */
 	public function getTranslation()
 	{
 		$output = '';
@@ -129,6 +203,7 @@ class Language extends CompilerLanguage {
 
 		$output .= PHP_EOL;
 		$output .= 'halt' . PHP_EOL.PHP_EOL;
+
 		$output .= $this->variables . PHP_EOL;
 
 		for($i = 0; $i <= $this->largestInteger; $i++)
@@ -137,15 +212,5 @@ class Language extends CompilerLanguage {
 		}
 
 		return $output;
-	}
-
-	public function setScope($scope)
-	{
-		$this->addMarker($scope.'_start');
-	}
-
-	public function removeScope($scope)
-	{
-		$this->addMarker($scope.'_end');
 	}
 }
