179b0c76e5fbdbf5c997d59292d0a925f96393a9
Loic Sharma <sharma.loic@gmail.com>
2013-03-21 13:20:21 -0400

    Renamed the sample file to source.php

--- compile.php
+++ compile.php
@@ -5,7 +5,7 @@ ini_set('xdebug.max_nesting_level', 2000);
 $autoloader = include 'vendor/autoload.php';
 $autoloader->add('Compiler', __DIR__.'/src/');
 
-$code = file_get_contents('sample.php');
+$code = file_get_contents('source.php');
 
 $parser = new PHPParser_Parser(new PHPParser_Lexer);
 $compiler = new Compiler\Compiler(new Compiler\Languages\Assembly\Language;);
