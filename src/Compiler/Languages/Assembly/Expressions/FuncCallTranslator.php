775f95ddf39f1b2e104e6457b564394ab0b70ee8
Loic Sharma <sharma.loic@gmail.com>
2013-03-21 01:22:47 -0400

    Added get_port and set_port functions

--- src/Compiler/Languages/Assembly/Expressions/FuncCallTranslator.php
+++ src/Compiler/Languages/Assembly/Expressions/FuncCallTranslator.php
@@ -6,8 +6,46 @@ class FuncCallTranslator extends Translator {
 
 	public function translate($expression)
 	{
+		$name = $expression->name->parts[0];
+
+		if($name == 'set_port')
+		{
+			$this->handleSetPort($expression);
+		}
+
+		elseif($name == 'get_port')
+		{
+			$this->handleGetPort($expression);
+		}
+
+		else
+		{
+			$this->handleFunctionCall();
+		}
+	}
+
+	public function handleSetPort($expression)
+	{
+		$port = $expression->args[0]->value->value;
+		$value = $expression->args[1]->value;
+
+		$value = $this->language->expressionToMemory($value);
+
+		$this->language->addCommand('out', $port, $value);
+	}
+
+	public function handleGetPort($expression)
+	{
 		$hash = 'a'.spl_object_hash($expression);
 
+		$port = $expression->args[0]->value->value;
+
+		$this->language->addCommand('in', $port, $hash);
+	}
+
+	public function handleFunctionCall($expression)
+	{
+		$hash = 'a'.spl_object_hash($expression);
 		$name = $expression->name->parts[0];
 
 		$marker = 'function_'.$name;
